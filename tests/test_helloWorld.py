d246c8c1c6bb3b0e114891aeb9bd4bdc290115e6
migo-robben <wuguohui@winsing.net>
2020-07-27 12:42:58 +0800

    test unittest file

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -1,9 +1,17 @@
 import unittest
+import maya.cmds as cmds
+help(cmds)
 
 
 class TestHelloWorld(unittest.TestCase):
+    def setUp(self):
+        self.transform = cmds.createNode("transform", name="testNode")
+
     def test_hello_world(self):
         a = 1
         b = 2
         c = a + b
-        self.assertEqual(c, 4, "Result Error.")
+        self.assertEqual(c, 3, "Result Error.")
+
+    def test_node_name(self):
+        self.assertEqual(self.transform, "testNode")
