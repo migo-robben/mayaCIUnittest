38d392603d1f8de21550679c60ffcf826b0e6c56
migo-robben <wuguohui@winsing.net>
2020-07-27 12:54:15 +0800

    add more unittest function

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -1,11 +1,14 @@
 import unittest
-import maya.cmds as cmds
-help(cmds)
+from maya import cmds
 
+from base import MayaBaseTestCase
 
+transform = None
 class TestHelloWorld(unittest.TestCase):
-    def setUp(self):
-        self.transform = cmds.createNode("transform", name="testNode")
+    @classmethod
+    def setUpClass(cls):
+        global transform
+        transform = cmds.createNode("transform", name="testNode")
 
     def test_hello_world(self):
         a = 1
@@ -14,4 +17,33 @@ class TestHelloWorld(unittest.TestCase):
         self.assertEqual(c, 3, "Result Error.")
 
     def test_node_name(self):
-        self.assertEqual(self.transform, "testNode")
+        self.assertEqual(transform, "testNode")
+
+    def test_node_exists(self):
+        """Test whether node was actually created."""
+        transform_exists = cmds.objExists(transform)
+        self.assertTrue(transform_exists)
+
+    def test_visibility_data_type(self):
+        """Test whether visibility attribute of node is a boolean."""
+        visibility = cmds.getAttr(transform + ".visibility")
+        self.assertIs(type(visibility), bool)
+
+    def test_has_roughly_correct_scale(self):
+        """Test whether node is roughly at the right scale."""
+        transform_scale = cmds.getAttr(transform + ".sx")
+        self.assertAlmostEqual(transform_scale, 1.00000001, places=7)
+
+    def test_movement(self):
+        """Test whether node can move."""
+        target_pos = [1, 2, 3.45]
+        cmds.xform(transform, translation=target_pos, worldSpace=True)
+        actual_pos = cmds.xform(
+            transform, query=True, translation=True, worldSpace=True
+        )
+        self.assertListEqual(actual_pos, target_pos)
+
+    def test_for_strange_attribute(self):
+        """Test for an expected Error."""
+        with self.assertRaises(ValueError):
+            cmds.getAttr(transform + ".whyWouldItHaveThisAttribute")
